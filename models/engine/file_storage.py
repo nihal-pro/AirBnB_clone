3f9f850afc1fd87e6b302d4c7dd40a225aff18a1
NIHAL <nihaleloudiki16@gmail.com>
2024-02-11 18:28:31 +0100

    handle task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,8 +31,9 @@ class FileStorage():
         """
         sets in __objects the obj with key <obj class name>.id
         """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        if obj:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """
